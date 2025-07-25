287ac64bc197aadfa91b88ea3c47c73a40912a70
AlvaroTF-git <alvaro.tferreir4@gmail.com>
2025-07-25 17:20:34 +0000

    jogodavelha2.0

--- jogodavelha.py
+++ jogodavelha.py
@@ -4,26 +4,31 @@ def interface():
     print(f" 1   [{tabuleiro[1][0]}] [{tabuleiro[1][1]}] [{tabuleiro[1][2]}]")
     print(f" 2   [{tabuleiro[2][0]}] [{tabuleiro[2][1]}] [{tabuleiro[2][2]}]")
 
-tabuleiro = ([['','','']],
-             [['','','']],
-             [['','','']]
-             )
+tabuleiro = [[' ',' ',' '],
+             [' ',' ',' '],
+             [' ',' ',' ']]
 rodada = "X"
+jogadas = 0
 
 while True:
+    if jogadas == 9:
+        print("O Jogo empatou!")
+        break
     interface()
     if rodada == "X":
         Linha = int(input("Digite a linha escolhida: "))
         Coluna = int(input("Digite a coluna escolhida: "))
         tabuleiro[Linha][Coluna] = "X"
+        jogadas = jogadas + 1
     interface()
     rodada = "O"
     if rodada == "O":
         Linha = int(input("Digite a linha escolhida: "))
         Coluna = int(input("Digite a coluna escolhida: "))
         tabuleiro[Linha][Coluna] = "O"
+        jogadas = jogadas + 1
     interface()
     rodada = "X"
-    break
+    
 
 
